156b6d7e8d5053effd6af308c45936a63d52c046
Jonathan Perry <jpper@umich.edu>
2023-11-09 11:54:41 -0600

    Implement notebook page ui

--- src/app/notebook/[noteId]/page.tsx
+++ src/app/notebook/[noteId]/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+import { clerk } from "@/lib/clerk-server";
 import { db } from "@/lib/db";
 import { $notes } from "@/lib/db/schema";
 import { auth } from "@clerk/nextjs";
 import { and, eq } from "drizzle-orm";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -26,10 +29,40 @@ const NotebookPage = async ({ params: { noteId } }: Props) => {
   if (notes.length != 1) {
     return redirect("/dashboard");
   }
+  const user = await clerk.users.getUser(userId);
 
   const note = notes[0];
 
-  return <pre>{JSON.stringify(note, null, 2)}</pre>
+  return (
+    <div className="min-h-screen grainy p-8">
+      <div className="max-w-4xl mx-auto">
+        <div className="border shadow-xl border-stone-200 rounded-lg p-4 flex items-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600" size={"sm"}>
+              Back
+            </Button>
+          </Link>
+
+          <div className="w-3"></div>
+          <span className="font-semibold">
+            {user.firstName} {user.lastName}
+          </span>
+          <span className="inline-clock mx-1">/</span>
+          <span className="text-stone-500 font-semibold">{note.name}</span>
+
+          {/* Delete Button */}
+          <div className="ml-auto">Delete button TODO</div>
+        </div>
+
+        {/* Spacer */}
+        <div className="h-4"></div>
+
+        <div className="border-stone-200 shadow-xl border rounded-lg px-16 py-8 w-full">
+          {/* Editor */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotebookPage;
