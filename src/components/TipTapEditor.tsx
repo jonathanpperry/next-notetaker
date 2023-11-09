ed5fa5bda97a0144eccbde5cb45fc2fb135a48cb
Jonathan Perry <jpper@umich.edu>
2023-11-09 16:18:06 -0600

    Add hint to instruct on how to use ai autocomplete

--- src/components/TipTapEditor.tsx
+++ src/components/TipTapEditor.tsx
@@ -1,27 +1,25 @@
 "use client";
-import React, { useEffect, useMemo, useRef, useState } from "react";
+import React from "react";
 import { EditorContent, useEditor } from "@tiptap/react";
 import { StarterKit } from "@tiptap/starter-kit";
 import TipTapMenuBar from "./TipTapMenuBar";
 import { Button } from "./ui/button";
 import { useDebounce } from "@/lib/useDebounce";
 import { useMutation } from "@tanstack/react-query";
-import { Text } from "@tiptap/extension-text";
-import { NoteType } from "@/lib/db/schema";
+import Text from "@tiptap/extension-text";
 import axios from "axios";
+import { NoteType } from "@/lib/db/schema";
 import { useCompletion } from "ai/react";
 
 type Props = { note: NoteType };
 
 const TipTapEditor = ({ note }: Props) => {
-  const [editorState, setEditorState] = useState(
+  const [editorState, setEditorState] = React.useState(
     note.editorState || `<h1>${note.name}</h1>`
   );
-
   const { complete, completion } = useCompletion({
     api: "/api/completion",
   });
-
   const saveNote = useMutation({
     mutationFn: async () => {
       const response = await axios.post("/api/saveNote", {
@@ -31,12 +29,11 @@ const TipTapEditor = ({ note }: Props) => {
       return response.data;
     },
   });
-
   const customText = Text.extend({
     addKeyboardShortcuts() {
       return {
         "Shift-a": () => {
-          //  Take the last 30 words
+          // take the last 30 words
           const prompt = this.editor.getText().split(" ").slice(-30).join(" ");
           complete(prompt);
           return true;
@@ -53,23 +50,19 @@ const TipTapEditor = ({ note }: Props) => {
       setEditorState(editor.getHTML());
     },
   });
+  const lastCompletion = React.useRef("");
 
-  const lastCompletion = useRef("");
-
-  useEffect(() => {
-    if (!editor || !editor) return;
-    // Get individual word so that we can insert into the editor
+  React.useEffect(() => {
+    if (!completion || !editor) return;
     const diff = completion.slice(lastCompletion.current.length);
     lastCompletion.current = completion;
-    editor?.commands.insertContent(diff);
+    editor.commands.insertContent(diff);
   }, [completion, editor]);
 
-  const debouncedState = useDebounce(editorState, 500);
-
-  useEffect(() => {
-    // Save to db
-    if (debouncedState === "") return;
-
+  const debouncedEditorState = useDebounce(editorState, 500);
+  React.useEffect(() => {
+    // save to db
+    if (debouncedEditorState === "") return;
     saveNote.mutate(undefined, {
       onSuccess: (data) => {
         console.log("success update!", data);
@@ -78,8 +71,7 @@ const TipTapEditor = ({ note }: Props) => {
         console.error(err);
       },
     });
-  }, [debouncedState]);
-
+  }, [debouncedEditorState]);
   return (
     <>
       <div className="flex">
@@ -89,9 +81,17 @@ const TipTapEditor = ({ note }: Props) => {
         </Button>
       </div>
 
-      <div className="prose">
+      <div className="prose prose-sm w-full mt-4">
         <EditorContent editor={editor} />
       </div>
+      <div className="h-4"></div>
+      <span className="text-sm">
+        Tip: Press{" "}
+        <kbd className="px-2 py-1.5 text-xs font-semibold text-gray-800 bg-gray-100 border border-gray-200 rounded-lg">
+          Shift + A
+        </kbd>{" "}
+        for AI autocomplete
+      </span>
     </>
   );
 };
